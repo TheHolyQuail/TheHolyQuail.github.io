50f3599717b7e089fc562d2b75b91640fee579e8
Eliot Wachtel <ewachtel@ucsc.edu>
2024-11-19 21:03:27 -0800

    Added prototype IOS detection.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // hi!
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  
+    if (isIOS) {
+      body.classList.add("ios-device");
+    //   root.style.setProperty("--can-render-pdf", "false");
+    } else {
+      body.classList.add("pdf-supported");
+    //   root.style.setProperty("--can-render-pdf", "true");
+    }
+  });
