d9ae7aceeaab5f740e7435ae958ac25b4c106679
Eliot Wachtel <ewachtel@ucsc.edu>
2024-11-19 22:25:50 -0800

    Added iPad check workaround... hopefully it works

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const body = document.body;
     const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  
-    if (isIOS) {
+    const isIPAD = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
+
+    if (isIOS || isIPAD)  {
       body.classList.add("ios-device");
     //   root.style.setProperty("--can-render-pdf", "false");
     } else {
